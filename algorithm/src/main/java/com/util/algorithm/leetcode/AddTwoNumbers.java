d5023eb36d6207d6950d9bbd68d8d21d30a7d192
shervin <shervin@shervin-Inspiron-5558o>
2017-07-08 23:15:26 +0530

    AddTwoNumbers & LongesSubString changes

--- algorithm/src/main/java/com/util/algorithm/leetcode/AddTwoNumbers.java
+++ algorithm/src/main/java/com/util/algorithm/leetcode/AddTwoNumbers.java
@@ -1,5 +1,8 @@
 package com.util.algorithm.leetcode;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 /**
  * @author shervin
  * 
@@ -40,16 +43,86 @@ public class AddTwoNumbers {
 			this.val = val;
 		}
 
+		@Override
+		public String toString() {
+			
+			StringBuilder builder = new StringBuilder();
+			builder.append(this.val);
+			ListNode curr = this.next;
+			while(curr != null) {
+				builder.append(" -> ");
+				builder.append(curr.val);
+				curr = curr.next;
+			}
+			return builder.toString();
+		}
+	}
+	
+	public ListNode getNodes(final int[] nodes) {
+		
+		ListNode headNode = new ListNode(0);
+		if(nodes != null && nodes.length > 0) {
+			ListNode nodeList = headNode;
+			for(int node : nodes) {
+				nodeList.next = new ListNode(node);
+				nodeList = nodeList.next;
+			}
+			return headNode.next;
+		}
+		return null;
+	}
+	
+	public ListNode addTwoNumbers(final ListNode l1, final ListNode l2) {
+		
+		ListNode dummyNode = new ListNode(0);
+		ListNode node1 = l1;
+		ListNode node2 = l2;
+		ListNode curnt = dummyNode;
+		int carry = 0;
+		while(node1 != null || node2 != null) {
+			int x = (node1 != null) ? node1.val : 0;
+			int y = (node2 != null) ? node2.val : 0;
+			int t = x + y + carry;
+			carry = t/10;
+			curnt.next = new ListNode(t % 10);
+			curnt = curnt.next;
+			if(node1 != null) {
+				node1 = node1.next;
+			}
+			if(node2 != null) {
+				node2 = node2.next;
+			}
+		}
+		if(carry > 0) {
+			curnt.next = new ListNode(carry);
+		}
+		return dummyNode.next;
+	}
+	
+	public static void main(String[] args) {
+		
+		AddTwoNumbers atn = new AddTwoNumbers();
+		ListNode node1 = atn.getNodes(new int[] {2, 4, 3});
+		System.out.println(node1);
+		ListNode node2 = atn.getNodes(new int[] {5, 6, 4});
+		System.out.println(node2);
+		ListNode sum = atn.addTwoNumbers(node1, node2);
+		System.out.println(sum);
 	}
 
-	/*public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
-	}*/
-
-	/*@Test
-	public void testTwoSum() {
-
-		assertArrayEquals(new int[] { 0, 1 }, twoSum1(new int[] { 2, 7, 11, 34 }, 9));
-		assertArrayEquals(new int[] { 2, 5 }, twoSum1(new int[] { 2, 6, 5, 23, 34, 4 }, 9));
-	}*/
+	@Test
+	public void testTwoSum1() {
+		
+		ListNode node1 = getNodes(new int[] {2, 4, 3});
+		ListNode node2 = getNodes(new int[] {5, 6, 4});
+		assertEquals(addTwoNumbers(node1, node2).toString(), "7 -> 0 -> 8");
+	}
+	
+	@Test
+	public void testTwoSum2() {
+		
+		ListNode node1 = getNodes(new int[] {0, 1});
+		ListNode node2 = getNodes(new int[] {0, 1, 2});
+		assertEquals(addTwoNumbers(node1, node2).toString(), "0 -> 2 -> 2");
+	}
 }
--- algorithm/src/main/java/com/util/algorithm/leetcode/LongestSubstring.java
+++ algorithm/src/main/java/com/util/algorithm/leetcode/LongestSubstring.java
@@ -2,6 +2,9 @@ package com.util.algorithm.leetcode;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 /**
@@ -23,31 +26,27 @@ import org.junit.Test;
  */
 public class LongestSubstring {
 
-	public int lengthOfLongestSubstring1(String s) {
-
-		String res = "";
-		int max = 0;
-		char[] chars = s.toCharArray();
-		for(char c : chars) {
-			res += c;
-			System.out.println(res);
-			if(s.contains(res) && res.length() > max) {
-				System.out.print("1");
-				max = res.length();
-				res = res.substring(1, res.length());
-			}else if(res.length()>0) {
-				System.out.print("2");
-			}
-		}
-		return max;
-	}
+	public int lengthOfLongestSubstring(final String s) {
+		int n = s.length();
+		int ans = 0;
+        Map<Character, Integer> charMap = new HashMap<>();
+        for (int j = 0, i = 0; j < n; j++) {
+            if (charMap.containsKey(s.charAt(j))) {
+                i = Math.max(charMap.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            charMap.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
 
 	@Test
 	public void testTwoSum() {
 
-		/*assertEquals(3, lengthOfLongestSubstring1("abcabcbb"));
-		assertEquals(1, lengthOfLongestSubstring1("bbbbbbb"));*/
-		assertEquals(3, lengthOfLongestSubstring1("pwwkew"));
+		assertEquals(3, lengthOfLongestSubstring("abcabcbb"));
+		assertEquals(1, lengthOfLongestSubstring("bbbbbbb"));
+		assertEquals(3, lengthOfLongestSubstring("pwwkew"));
+		assertEquals(6, lengthOfLongestSubstring("characters"));
 		
 	}
 }
